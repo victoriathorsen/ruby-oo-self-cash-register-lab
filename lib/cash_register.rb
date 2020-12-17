03e8b8aaad9d5d84780f6de880770717e6d4f7fc
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-17 15:39:43 -0800

    commit tree 1e59dc816bb2d3c0e0f6cc8c75edb9457380d404

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
 
+class CashRegister
+    
+    attr_accessor :total, :items, :discount
+
+    def initialize (discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+   def add_item (item, price, quantity=1)
+        quantity.times do 
+            self.items << item
+        end
+        @last_total = self.total  #just using the instance variable of last_totaal which is why we dont have a reader
+        self.total += price * quantity 
+   end
+
+   def apply_discount
+       if self.discount != 0 
+            self.total -= (self.total * self.discount.to_f / 100).to_i
+            "After the discount, the total comes to $#{self.total}."
+        else 
+            "There is no discount to apply."
+       end
+    end
+
+    def void_last_transaction
+       self.total = @last_total   #can use instance variable to look directly here and dont need it outside this
+    end
+    
+
+end
